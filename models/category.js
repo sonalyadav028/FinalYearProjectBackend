503d00bc6b7e2386f690f135758d1640bd744abe
Sonal <sonalyadav019@gmail.com>
2021-08-02 14:33:40 +0530

    Initial commit

---
+++ models/category.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const { stringify } = require('uuid');
+
+const categorySchema = new mongoose.Schema ({
+
+    name:{
+        type: String,
+        trim: true,
+        required: true,
+        maxlength: 32,
+        unique: true
+    }
+}, {timesstamps : true});
+
+module.exports = mongoose.model("Category" , categorySchema);
